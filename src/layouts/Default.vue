45230f27f3438eec104bc81b8d75ca56cdab4b2e
David Couronné <couronne.david@gmail.com>
2019-11-04 10:44:37 +0100

    style(): upgrade Default.vue

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,64 +1,38 @@
 <template>
-  <div>
-    <v-toolbar>
-      <v-toolbar-title>
-        <g-link to="/">
-          {{ $static.metaData.siteName }}
-        </g-link>
-      </v-toolbar-title>
-      <v-spacer />
-      <nav class="nav">
-        <g-link
-          class="nav__link"
-          to="/"
+  <v-app>
+    <div>
+      <v-app-bar>
+        <v-btn
+          icon
+          router
+          exact
+          :to="$url('/')"
         >
+          <v-icon>mdi-home</v-icon>
+        </v-btn>
+        <v-toolbar-title>
+          {{ $static.metadata.siteName }}
+        </v-toolbar-title>
+        <v-spacer />
+
+        <v-btn :to="$url('/')">
           Home
-        </g-link>
-        <g-link
-          class="nav__link"
-          to="/about"
-        >
+        </v-btn>
+        <v-btn :to="$url('/about')">
           About
-        </g-link>
-      </nav>
-    </v-toolbar>
-    <slot />
-  </div>
+        </v-btn>
+      </v-app-bar>
+    </div>
+    <v-content>
+      <slot />
+    </v-content>
+  </v-app>
 </template>
 
 <static-query>
-query {
-  metaData {
-    siteName
+  query {
+    metadata {
+      siteName
+    }
   }
-}
 </static-query>
-
-<style>
-body {
-  font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    'Helvetica Neue', Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
-}
-
-.nav__link {
-  margin-left: 20px;
-}
-</style>
